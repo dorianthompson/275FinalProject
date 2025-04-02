7de61ae3f3cbb4043536d0fbaa5d598104e7db7a
Daniel Fazzari <dfazzari@udel.edu>
2025-04-02 15:08:57 -0400

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import { Button, Form } from 'react-bootstrap';
+//import { Button, Form } from 'react-bootstrap';
 import { Homepage }  from './Components/Homepage'
 import { HomeButton }  from './Components/HomeButton'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -9,26 +9,26 @@ import { DetailedQuiz } from './Components/DetailedQuiz';
 
 
  //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
- let keyData = "";
- const saveKeyData = "MYKEY";
- const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
- if (prevKey !== null) {
+ //let keyData = "";
+ //const saveKeyData = "MYKEY";
+ //const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+ /*if (prevKey !== null) {
    keyData = JSON.parse(prevKey);
- }
+ } */
 
 function App() {
-  const [key, setKey] = useState<string>(keyData); //for api key input
+  //const [key, setKey] = useState<string>(keyData); //for api key input
  
    //sets the local storage item to the api key the user inputed
-   function handleSubmit() {
+   /*function handleSubmit() {
      localStorage.setItem(saveKeyData, JSON.stringify(key));
      window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-   }
+   } */
  
    //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
-   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
+   /*function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
      setKey(event.target.value);
-   }
+   } */
   return (
     <Router>
       <div className="App">
@@ -40,7 +40,6 @@ function App() {
             <Route path="/detailed" element={<DetailedQuiz />} />
           </Routes>
         </header>
-        <Form>{/* Your form content */}</Form>
       </div>
     </Router>
   );
