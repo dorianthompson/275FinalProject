ee4a011ec87c4f6276339db588142897b89f0c11
Dorian <thompsondorian2@gmail.com>
2025-04-11 20:32:02 -0400

    Added dynamically updating progress for basic quiz

--- src/Components/BasicQuiz/BasicQuiz.tsx
+++ src/Components/BasicQuiz/BasicQuiz.tsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
+import { useState } from "react";
 
 const BasicAssessmentQuestions = [
   'I thrive in a quiet work environment.',
@@ -25,10 +26,18 @@ const BasicAssessmentQuestions = [
  
  //make sure you can only check one
  //how to store data...dictionary key = idx, value which is checked
+ //setProgess needs to check if that question has been selected if not then update the  progress to show completion
  
- 
+ const checkedSet = new Set<number>([]);
 export function BasicQuiz() {
-    const progress = 40;
+    const [progress, setProgress] = useState<number>(0);
+
+    const handleClick = ((id:number) =>{
+      if(!checkedSet.has(id) ){
+        checkedSet.add(id);
+        setProgress(Math.floor((checkedSet.size / BasicAssessmentQuestions.length) * 100));
+      }
+    })
     
      
     return (
@@ -55,6 +64,8 @@ export function BasicQuiz() {
             label={choice}
             type="radio"
             name="radio"
+            onClick={() => {
+              handleClick(idx+1)}}
           />
         </div>
       ))}
